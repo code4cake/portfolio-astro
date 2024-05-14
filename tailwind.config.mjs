5c68de6d102f6fb6a5eebe8cca9276f8cefd2662
DS <ds@DSs-MacBook-Pro.local>
2024-05-14 16:43:45 +0200

    Add mobile layout for landing page.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "black-muted": "#071c20",
+      },
+      fontSize: {
+        base: "1rem",
+        large: "1.33rem",
+        xl: "4.21rem",
+        "2xl": "5.61rem",
+      },
+    },
+  },
+  plugins: [],
 }
+
+// 20 ~h6 24px - large
+// 64 ~h2 76px - xl
+// 100 ~h1 101px - 2xl
