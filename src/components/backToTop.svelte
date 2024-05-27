ad5250b6eb8757e96de6a6ba2e2065f40818e39d
DS <ds@DSs-MacBook-Pro.local>
2024-05-27 17:08:03 +0200

    feat/about - create about page

---
+++ src/components/backToTop.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let showAtPixel = 0
+
+  let scrollHeight = 0
+
+  const gotToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    })
+  }
+
+  $: showGoToTop = scrollHeight > showAtPixel
+</script>
+
+{#if showGoToTop}
+  <button
+    class="pb-8 text-right text-large uppercase border-none hover:-translate-y-2"
+    on:click={gotToTop}>back to to top</button
+  >
+{/if}
+
+<svelte:window bind:scrollY={scrollHeight} />
