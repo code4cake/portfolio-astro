60866cc1688aeaeb378250987b2293d7db7dbe60
DS <ds@DSs-MacBook-Pro.lan>
2024-06-12 16:10:47 +0200

    feat/work - fix typos

--- src/components/projectList.svelte
+++ src/components/projectList.svelte
@@ -29,28 +29,4 @@
       <span class="fira__regular pr-8 text-large">{project.year}</span>
     </li>
   {/each}
-  <!-- <li class="flex border-b-2 border-black-muted">
-    <a
-      href=""
-      class="fira__regular ml-8 text-2xl uppercase leading-tight md:pr-2 md:text-3xl"
-      >0. dpt omgeving
-    </a>
-    <span class="fira__regular pr-8 text-large">2023</span>
-  </li>
-  <li class="flex border-b-2 border-black-muted">
-    <a
-      href=""
-      class="fira__regular pl-8 text-2xl uppercase leading-tight md:pr-2 md:text-3xl"
-      >1. smilng agency
-    </a>
-    <span class="fira__regular pr-8 text-large">2022</span>
-  </li>
-  <li class="flex border-b-2 border-black-muted">
-    <a
-      href=""
-      class="fira__regular pl-8 text-2xl uppercase leading-tight md:pr-2 md:text-3xl"
-      >2. Cristy bertolini
-    </a>
-    <span class="fira__regular pr-8 text-large">Coming Soon</span>
-  </li> -->
 </ul>
